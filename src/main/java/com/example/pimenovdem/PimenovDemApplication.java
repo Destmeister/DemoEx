716ab1edb538dbdf856b40f2cbdfe97cbfa0ad23
unknown <Student@305-15.spobip-bel.ru>
2024-02-03 14:14:44 +0300

    first commit

---
+++ src/main/java/com/example/pimenovdem/PimenovDemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pimenovdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimenovDemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PimenovDemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pimenovdem/PimenovDemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pimenovdem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimenovDemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
